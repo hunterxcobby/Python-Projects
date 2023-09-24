cad8b0165cf25c10c606ace871f5b08dac1536e9
hunterxcobby <solomonsefah13@gmail.com>
2023-09-24 19:30:55 +0000

    a demonstration of using variables to save values

---
+++ lessons/exercises/5-variables1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#Asks user for name and saves it in a variable
+name = input("What is your name?")
+
+#prints users name to screen 
+print(name)
+
+#save the string length of name and save it in a variable
+length = len(name)
+
+#prints the length of the user's name
+print(length)
