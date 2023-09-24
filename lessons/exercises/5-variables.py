d658344354e7a7b0f58807e32ae6a63a929cad89
hunterxcobby <solomonsefah13@gmail.com>
2023-09-24 17:43:29 +0000

    A file to illustrate variables

---
+++ lessons/exercises/5-variables.py
@@ -0,0 +1,14 @@
+# Variable Example Program
+
+# Step 1: Prompt the user for their name
+name = input("What is your name? ")
+
+# Step 2: Prompt the user for their age
+age = int(input("How old are you? "))
+
+# Step 3: Create a personalized greeting
+greeting = f"Hello, {name}! You are {age} years old."
+
+# Step 4: Print the greeting
+print(greeting)
+
