3c162c493aed677f630256b65d967b124d0761ff
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2023-09-24 17:09:50 +0000

    Update 4-input_function.py
    Added comments to explain why the third line is commented out

--- lessons/exercises/4-input_function.py
+++ lessons/exercises/4-input_function.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
+#input("What is your name?")
+#This line has been coomented out so you understand how the input function works
+#That line of code is easily replaced with the output but it works behind the scenes
+#when you uncomment it, you realise the prompt appears twice
+
 print ("Hello" + " "+ input("What is your name?"))
