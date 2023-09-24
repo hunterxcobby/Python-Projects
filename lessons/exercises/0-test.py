fb8d4e1f50b5bdd81f841427c7c65284ce41384b
ronsig30 <ronaldrogony30@gmail.com>
2023-09-24 20:37:33 +0300

    Addition of new line at the end of code

--- lessons/exercises/0-test.py
+++ lessons/exercises/0-test.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print ("Hello World, let us do great things together!")
+print ("Hello World, let us do great things together!\n")
--- lessons/exercises/1-funny.py
+++ lessons/exercises/1-funny.py
@@ -8,4 +8,4 @@ for i in range(1,11):
         print ("WHERE ARE ALL THE BRACKETS!??")
     else:
         print ("HOW DO YOU PEOPLE READ THIS SYNTAX EASILY?")
-    
+   print("\n") 
