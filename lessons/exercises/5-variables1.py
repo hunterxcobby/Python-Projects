ddf31076c2560a64bbca2990f99fde25e0fe0f38
EMarvelM <marvelousegbe19@gmail.com>
2023-09-25 17:08:42 +0100

    Use f-string to print user's name and length on the same line as space is also required after a prompt

--- lessons/exercises/5-variables1.py
+++ lessons/exercises/5-variables1.py
@@ -3,11 +3,22 @@
 #Asks user for name and saves it in a variable
 name = input("What is your name? ")
 
+#make first letter a Block(upper)
+i = 0
+for c in name:
+    if i == 0:
+        c = c.upper()
+        new_name = c
+        i += 1
+    else:
+        c = c.lower()
+        new_name += c
+
 #prints users name to screen
-print(f"{name}", end=" ")
+print(f"{new_name}", end=" ")
 
 #save the string length of name and save it in a variable
-length = len(name)
+length = len(new_name)
 
 #prints the length of the user's name
 print(f"your name is {length} characters long")
