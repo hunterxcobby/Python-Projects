383e486925a3a583d4b829acd09695a1d979db11
hunterxcobby <solomonsefah13@gmail.com>
2023-09-29 16:04:03 +0000

    to demonstatrate the removal of a newline

--- lessons/exercises/6-print_name.py
+++ lessons/exercises/6-print_name.py
@@ -4,10 +4,10 @@ first_name = "Cobby"
 surname = "Sefah"
 
 # display my full name
-print(first_name, surname)
+print(first_name, surname, end=" ")
 
 # print multiple things
-print("comes from", end="\n")
-print("Ghana", end="\n")
-print("stays in", end="\n")
-print("Accra", end="\n")
+print("comes from", end=" ")
+print("Ghana", end=" ")
+print("and stays in", end=" ")
+print("Accra.", end=" ")
