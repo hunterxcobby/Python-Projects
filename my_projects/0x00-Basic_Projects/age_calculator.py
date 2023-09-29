a5c012bb925d3ee4e9341b86070a795f836ec1e3
hunterxcobby <solomonsefah13@gmail.com>
2023-09-29 18:04:08 +0000

    asked for current age

--- my_projects/0x00-Basic_Projects/age_calculator.py
+++ my_projects/0x00-Basic_Projects/age_calculator.py
@@ -5,9 +5,12 @@ year_of_birth = input("Year of birth: ")
 month_of_birth = input("Month of birth (0-12): ")
 day_of_birth = input("Day of the month: ")
 
+# current year
+current_year = input("Which year are you?")
+
 # Display details
 print("Your date of birth is:", end=" ")
 print(day_of_birth, month_of_birth, year_of_birth, sep="-")
 
-# Working on the age
-current_year = 2023
+# Calculate the age
+age = current_year - year_of_birth
