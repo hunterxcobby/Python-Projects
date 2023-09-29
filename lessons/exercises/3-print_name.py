304000b74f330f8d08ddf31fa6dc8e54527c0f16
hunterxcobby <solomonsefah13@gmail.com>
2023-09-29 21:46:10 +0000

    changed name to 3_print_name.py

---
+++ lessons/exercises/3-print_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+first_name = "Cobby"
+surname = "Sefah"
+
+# display my full name
+
+# print multiple things
+# print(values, ..., end= "\n")
+print(first_name, surname, end=" ")
+print("comes from", end=" ")
+print("Ghana", end=" ")
+print("and stays in", end=" ")
+print("Accra.", end="\n")
+
+# print(values, ..., sep= " ")
+print(first_name, surname, sep="")
+ 
+# always remember your commas
+# all positional arguments come 
+# before keyword arguments
