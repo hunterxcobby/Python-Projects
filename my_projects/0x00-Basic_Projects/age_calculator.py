090174bdca09147734da59077fa5c9f4b8f3f1e8
hunterxcobby <solomonsefah13@gmail.com>
2023-09-29 18:15:05 +0000

    imported datetime for current year

--- my_projects/0x00-Basic_Projects/age_calculator.py
+++ my_projects/0x00-Basic_Projects/age_calculator.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 
+#to get cuurent year
+import datetime
+
 # Ask for details
 year_of_birth = input("Year of birth: ")
 month_of_birth = input("Month of birth (0-12): ")
 day_of_birth = input("Day of the month: ")
 
 # current year
-current_year = input("Which year are you? ")
+current_year = datetime.datetime.now().year
 
 # Display details
 print("Your date of birth is:", end=" ")
 print(day_of_birth, month_of_birth, year_of_birth, sep="-")
 
 # Calculate the age
-age = int(current_year) - int(year_of_birth)
+age = current_year - int(year_of_birth)
 
 # Display age
-print("You are ", age, end=" ")
-print("years old.")
+print("You are", age, "years old.")
