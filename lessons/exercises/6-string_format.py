2388469173daf077cbb4510d2ccf59e75de170ee
hunterxcobby <solomonsefah13@gmail.com>
2023-09-30 15:32:53 +0000

    added format specifiers to explain new formatting style

--- lessons/exercises/6-string_format.py
+++ lessons/exercises/6-string_format.py
@@ -9,7 +9,12 @@ age = int(input("Age: "))
 details = "Your name is %s %s and you are %d years old" %(fname, surname,age)
 print(details)
 
-print
+print()
 # Using the new style formatting (str.format{})
 details = "Your name is {} {} and you are {} years old".format(fname, surname,age)
+print(details)
+
+print()
+# Using specifiers in the new formatting style
+details = "Your name is {:s} {:s} and you are {:d} years old".format(fname, surname,age)
 print(details)
