8af829bc06ccab4d6899d6493ca7864761a6b633
hunterxcobby <solomonsefah13@gmail.com>
2023-09-30 15:56:23 +0000

    formatting using the f-string

--- lessons/exercises/6-string_format.py
+++ lessons/exercises/6-string_format.py
@@ -20,11 +20,16 @@ details = "Your name is {:s} {:s} and you are {:d} years old".format(fname, surn
 print(details)
 
 print()
-# Using their indices
+# Using indices in the new formatting style
 details = "Your name is {1} {0} and you are {1} years old".format(fname, surname,age)
 print(details)
 
 print()
-# Using keywords
+# Using keywords in the new formatting style
 details = "Your name is {fn} {ln} and you are {ag} years old".format(fn = fname, ln = surname, ag = age)
+print(details)
+
+print()
+# Using the f-string formatting style
+details = f"Your name is {fname} {surname} and you are {age} years old"
 print(details)
