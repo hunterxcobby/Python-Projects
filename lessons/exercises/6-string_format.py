d2fd07fa05197edcdef0129280e7abea5b0cc0e9
hunterxcobby <solomonsefah13@gmail.com>
2023-09-30 15:17:14 +0000

    removed the variable

--- lessons/exercises/6-string_format.py
+++ lessons/exercises/6-string_format.py
@@ -5,5 +5,4 @@ surname = input("Surname: ")
 age = int(input("Age: "))
 
 # Display details
-message = "Your name is %s %s and you are %d years old" %(fname, surname, age)
-print(message)
+print("Your name is %s %s and you are %d years old" %(fname, surname,age))
