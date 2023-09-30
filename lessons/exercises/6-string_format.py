710c016aa4a9d9bbce654c06f689e6c4e57116fb
hunterxcobby <solomonsefah13@gmail.com>
2023-09-30 15:15:10 +0000

    added a file to explain string formatters

---
+++ lessons/exercises/6-string_format.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+fname = input("Firstname: ")
+surname = input("Surname: ")
+age = int(input("Age: "))
+
+# Display details
+message = "Your name is %s %s and you are %d years old" %(fname, surname, age)
+print(message)
