68f1280ece14f124bcc37f4b7a94b73801fecb1c
hunterxcobby <solomonsefah13@gmail.com>
2023-09-30 15:28:09 +0000

    used the new style of formatting

--- lessons/exercises/6-string_format.py
+++ lessons/exercises/6-string_format.py
@@ -5,5 +5,11 @@ surname = input("Surname: ")
 age = int(input("Age: "))
 
 # Display details
+# Using old style formatting
 details = "Your name is %s %s and you are %d years old" %(fname, surname,age)
+print(details)
+
+print
+# Using the new style formatting (str.format{})
+details = "Your name is {} {} and you are {} years old".format(fname, surname,age)
 print(details)
