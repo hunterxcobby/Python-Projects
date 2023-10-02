0009e361500a7c13f7582c8876b0837532857596
hunterxcobby <solomonsefah13@gmail.com>
2023-10-02 21:55:20 +0000

    else if statements

--- lessons/exercises/7-if_else.py
+++ lessons/exercises/7-if_else.py
@@ -48,3 +48,13 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
+
+
+# One line if statement:
+if a > b: print("a is greater than b")
+
+# One line if else statement:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
