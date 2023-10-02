016f48a421d6681e9889eb982d6477eff5221e24
hunterxcobby <solomonsefah13@gmail.com>
2023-10-02 22:28:40 +0000

    implemented 'and' 'or'

--- lessons/exercises/8-logical_operators.py
+++ lessons/exercises/8-logical_operators.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
-# You can also have multiple else statements on the same line:
+# The "and" keyword is a logical operator, and is used to combine conditional statements:
+# Test if a is greater than b, AND if c is greater than a:
 a = 200
-b = 200
-print("A") if a > b else print("=") if a == b else print("B")
+b = 50
+c = 300
+if a > b and c > a:
+    print("Both conditions are true")
 
+# The or keyword is a logical operator, and is used to combine conditional statements:
+# Test if a is greater than b, OR if a is greater than c:
+a = 200
+b = 300
+c = 50
+if a > b or a > c:
+    print("At least one of the conditons is true")
