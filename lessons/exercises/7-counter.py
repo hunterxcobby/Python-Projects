9c90e23d336349eee7dbbc8fc0d841fcb798d762
hunterxcobby <solomonsefah13@gmail.com>
2023-10-03 16:13:11 +0000

    added a file to explain the implementation of a counter

---
+++ lessons/exercises/7-counter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Prints from 1 to 10"""
+
+counter = 1 # Initialization
+
+while (counter <= 10): # condition
+    """WIll only execute if the condition is true"""
+    print(counter)
+    counter = counter + 1 # increment/decrement
+
+# the increment depends on the condtion you want to provide
+print("Your loop finished executing") 
+
