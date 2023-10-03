b04c43bd6d469989ae93a8ea00b9c8bf262fc4c5
hunterxcobby <solomonsefah13@gmail.com>
2023-10-03 16:58:23 +0000

    made file executable

---
+++ lessons/exercises/7-counter1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Prints from 1 to 10"""
+
+counter = 1 # Initialization
+
+while (counter <= 10): # condition
+    """WIll only execute if the condition is true"""
+    if (counter == 4):
+        pass
+    elif (counter == 5):
+        print("five")
+    else:
+        print(counter)
+
+    counter = counter + 1 # increment/decrement
+
+# the increment depends on the condtion you want to provide
+print("Your loop finished executing") 
+
