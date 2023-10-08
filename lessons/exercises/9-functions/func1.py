6196637667380e9ab31bad6e8b9075183ad33b9e
hunterxcobby <solomonsefah13@gmail.com>
2023-10-08 11:26:13 +0000

    defined a function called age_calc

---
+++ lessons/exercises/9-functions/func1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# name = "Cobby Sefah"
+# age = 21
+
+# Defining the function
+def age_calc(name, yob, current_year):
+    age = current_year - yob
+    print(f"{name}, you are {age} years old")
+
+# Calling the function
+age_calc("Cobby", 2002, 2023)
