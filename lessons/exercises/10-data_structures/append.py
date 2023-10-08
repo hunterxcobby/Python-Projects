4f4903c6a5a90eefd173236db5fbae68a950e24b
hunterxcobby <solomonsefah13@gmail.com>
2023-10-08 17:09:35 +0000

    deleted this file

--- lessons/exercises/10-data_structures/append.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-myList = ["Cobby", 21, 5.9]
-names = ["marvel", "cobby", "ami", "ayomide"]
-nums = [4, 7, 1, 34, 10, 8]
-
-# adding 20 to the end of the list
-nums.append(20)
-print(nums)
