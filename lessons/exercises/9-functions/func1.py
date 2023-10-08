ad97fbcefb0e852631bb2c3104d97303e43adcc5
hunterxcobby <solomonsefah13@gmail.com>
2023-10-08 11:45:33 +0000

    explained keyword and positional arguments

--- lessons/exercises/9-functions/func1.py
+++ lessons/exercises/9-functions/func1.py
@@ -8,6 +8,9 @@ def age_calc(name, yob, current_year): # parameters(placeholderss)
     age = current_year - yob
     print(f"{name}, you are {age} years old")
 
-# Calling the function
+# Calling the function (positional arguments)
 age_calc("Cobby", 2002, 2023) # arguments(values)
 age_calc("Ami", 2003, 2023)
+
+# Calling the function (keyword arguments)
+age_calc(yob=2002, current_year=2023, name="Cobby")
