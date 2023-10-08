5915feb1b94e189982a4805f7e9b8fe35f3435c6
hunterxcobby <solomonsefah13@gmail.com>
2023-10-08 11:29:38 +0000

    added a second person

--- lessons/exercises/9-functions/func1.py
+++ lessons/exercises/9-functions/func1.py
@@ -4,9 +4,10 @@
 # age = 21
 
 # Defining the function
-def age_calc(name, yob, current_year):
+def age_calc(name, yob, current_year): # parameters(placeholderss)
     age = current_year - yob
     print(f"{name}, you are {age} years old")
 
 # Calling the function
-age_calc("Cobby", 2002, 2023)
+age_calc("Cobby", 2002, 2023) # arguments(values)
+age_calc("Ami", 2003, 2023)
