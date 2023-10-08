585af96a78dcb77bff5f7420edbfe20e097ceaac
hunterxcobby <solomonsefah13@gmail.com>
2023-10-08 15:11:56 +0000

    added a file to explain how the sorted and len functin works

---
+++ lessons/exercises/10-data_structures/len_sort.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+myList = ["Cobby", 21, 5.9]
+names = ["marvel", "cobby", "ami", "ayomide"]
+nums = [4, 7, 1, 34, 10, 8]
+
+# to know the number of elements
+print(len(myList))
+print(len(names))
+print(len(nums))
+
+# to sort the elements
+print(sorted(nums))
+
