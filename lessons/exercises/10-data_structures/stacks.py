b29315dea6478e2c67640f1c0a6257eab7cb4fcf
hunterxcobby <solomonsefah13@gmail.com>
2023-10-09 22:11:11 +0000

    implemetend a stack with a list

---
+++ lessons/exercises/10-data_structures/stacks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+   Using lists as stacks
+   A stack is a data structure that follows the LIFO principal
+   that means last in, first out.
+   we can use lists to implemet that
+'''
+
+'''We have the push: Adding an element to the top of the stack'''
+stack = []
+stack.append(1) # Pushes 1 onto the stack
+stack.append(2) # Pushes 2 onto the stack
+
+print(stack)
+
+'''We have the pop: Removes and return the last element of a stack'''
+item = stack.pop() # Removes the last if no index is passes to it
+print(item)
+
+'''Thus, this is a simple implemetation of stacks'''
