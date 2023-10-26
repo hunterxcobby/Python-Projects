bc64ce12d1b456f37e814f39f4af90be0359527a
hunterxcobby <solomonsefah13@gmail.com>
2023-10-26 13:59:11 +0000

    add a module docstring

---
+++ lessons/exercises/13-TDD/0-module_docstring.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This module provides functions for basic arithmetic operations.
+
+Functions:
+- add(a, b): Adds two numbers.
+- subtract(a, b): Subtracts b from a.
+- multiply(a, b): Multiplies two numbers.
+- divide(a, b): Divides a by b.
+"""
