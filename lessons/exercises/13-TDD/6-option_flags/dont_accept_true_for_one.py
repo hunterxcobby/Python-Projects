a1e805713532f743e464a07101975e8ba817aa54
hunterxcobby <solomonsefah13@gmail.com>
2023-10-27 18:24:33 +0000

    dont accept true for 1

--- lessons/exercises/13-TDD/6-option_flags/dont_accept_true_for_one.py
+++ lessons/exercises/13-TDD/6-option_flags/dont_accept_true_for_one.py
@@ -3,7 +3,8 @@
 def is_even(n):
     """
     Check if a number is even.
-
+    >>> is_even(1)
+    False
     >>> is_even(2)
     True
     >>> is_even(3)
@@ -13,7 +14,7 @@ def is_even(n):
 
 if __name__ == '__main__':
     import doctest
-    doctest.testmod(verbose=True, optionflags=doctest.DONT_ACCEPT_TRUE_FOR_1)
+    doctest.testmod(verbose=True,optionflags=doctest.DONT_ACCEPT_TRUE_FOR_1)
 
 """In this example, we have a function is_even that checks if a number is even. 
 The doctests show that is_even(2) should return True, 
