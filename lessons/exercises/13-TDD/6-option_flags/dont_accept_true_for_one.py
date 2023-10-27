1938a7a5b488e4a6feade420bb849c84e8f22705
hunterxcobby <solomonsefah13@gmail.com>
2023-10-27 18:22:48 +0000

    dont accept true for 1

---
+++ lessons/exercises/13-TDD/6-option_flags/dont_accept_true_for_one.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def is_even(n):
+    """
+    Check if a number is even.
+
+    >>> is_even(2)
+    True
+    >>> is_even(3)
+    False
+    """
+    return n % 2 == 0
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True, optionflags=doctest.DONT_ACCEPT_TRUE_FOR_1)
+
+"""In this example, we have a function is_even that checks if a number is even. 
+The doctests show that is_even(2) should return True, 
+and is_even(3) should return False. With DONT_ACCEPT_TRUE_FOR_1, 
+the flag disables the automatic conversion of 1 to True,
+so is_even(2) will be tested as expected."""
