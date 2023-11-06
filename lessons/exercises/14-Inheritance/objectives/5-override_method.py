f1d9cbae300bb44ffe773d257733ce9cb77d7760
hunterxcobby <solomonsefah13@gmail.com>
2023-11-06 16:39:21 +0000

    add comment

--- lessons/exercises/14-Inheritance/objectives/5-override_method.py
+++ lessons/exercises/14-Inheritance/objectives/5-override_method.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
+"""Imagine you have a robot that can talk, 
+but you want your special robot to talk in a cooler way. 
+You can change the way it talks by writing your own 
+version of the talk method.
+
+Python allows you to do this by overriding the method. 
+This means you provide a new definition for the method 
+in your special robot. When you call the talk method 
+on your special robot, it uses your custom version 
+instead of the default one.
+"""
 class SimpleRobot:
     def talk(self):
         print("I can talk")
@@ -12,3 +23,4 @@ simple_robot = SimpleRobot()
 simple_robot.talk()
 special_robot = SpecialRobot()
 special_robot.talk()  # This will print "I can talk in a special way!"
+
