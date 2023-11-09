c282ad6444d625a743049198a3baf0369c2bde8b
hunterxcobby <solomonsefah13@gmail.com>
2023-11-09 14:16:50 +0000

    add comments

--- lessons/exercises/16-args_n_kwargs/arbituary_kw_args.py
+++ lessons/exercises/16-args_n_kwargs/arbituary_kw_args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 
+# Defining a function named 'infoperson' that accepts a variable number of positional arguments (*args)
+# and a variable number of keyword arguments (**kwargs).
 def infoperson(*args, **kwargs):
-    for key,value in kwargs.items(): #we need to use this method since it creates a dict
-        print(key,value, sep=": ")
+    # Loop through the keyword arguments and print each key-value pair.
+    for key, value in kwargs.items(): #we need to use this method since it creates a dict
+        print(key, value, sep=": ")
     
+    # Loop through the positional arguments and print each value.
     for i in args:
         print(i, end=": ")
     print()
 
+# Calling the 'infoperson' function with a combination of positional and keyword arguments.
 # remember that the arbituary position arg comes before the keyword in the function
-infoperson(12, 3, 4,name="Cobby", age=21, dept="CSE")
-infoperson(54, 15, 89,name="Ami", age=20, dept="CSE")
+infoperson(12, 3, 4, name="Cobby", age=21, dept="CSE")
+infoperson(54, 15, 89, name="Ami", age=20, dept="CSE")
