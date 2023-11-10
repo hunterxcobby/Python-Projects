6cb6daca49f9f272cd0a46b3fbb3473d691c8462
hunterxcobby <solomonsefah13@gmail.com>
2023-11-10 13:21:31 +0000

    implementing how to load a json file

---
+++ lessons/exercises/17-JSON/load_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import json
+
+file = "user.json"
+
+with open(file, "r") as json_file:
+    data = json.load(json_file)
+    print(data["name"])
+    name = data["name"]
+    age = data['Age']
+    hobbies = data['Hobbies']
+    print(f"{name} is {age} years old and his hobbies are", end=" ")
+
+    for hobbies in data['Hobbies']:
+        print(hobbies, end=", ")
+    print()
+    for friends in data["Friends"]:
+        print(f"{data['name']} and {friends['name']} are best friends " )
+        sub_friends = friends['Friends'][1]
+        print(f"{friends['name']} and {sub_friends} are friends " )
+    
+
+
+
