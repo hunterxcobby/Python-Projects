634e3fc6416d2cac9cfb962d3141467646538881
hunterxcobby <solomonsefah13@gmail.com>
2023-11-10 13:52:10 +0000

    modify this code

--- lessons/exercises/17-JSON/create_json.py
+++ lessons/exercises/17-JSON/create_json.py
@@ -2,17 +2,24 @@
 
 import json
 
+# Function to write data to a JSON file with indentation
+def write_json(data, filename="user2.json"):
+    """
+    Writes the given data to a JSON file.
 
-
-
-# Let us write a function that will put the data into a json file
-def write_json(data, filename= "user2.json"):
+    Args:
+        data (dict): The data to be written to the file.
+        filename (str): The name of the file. Default is "user2.json".
+    """
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
+# Source JSON file
 file_from = "user.json"
 
+# Read data from the source file
 with open(file_from, "r") as json_file:
-    data = json.load(json_file)# this is where the magic starts
+    data = json.load(json_file)  # Load data from the source file
 
+# Call the function to write data to a new JSON file
 write_json(data)
