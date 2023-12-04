b5defbc21dedb1cf9e861c0295c5a58acbb575f9
hunterxcobby <solomonsefah13@gmail.com>
2023-12-04 15:38:06 +0000

    add a method

--- my_projects/airbnb_console_test/console.py
+++ my_projects/airbnb_console_test/console.py
@@ -3,7 +3,12 @@ import cmd
 
 class myConsole(cmd.Cmd):
     prompt = "cmd$ "
-    pass
+    
+    def do_create(self, input):
+        print("I have created",input)
+
+    def do_exit(self, ):
+        exit()
 
 if __name__ == '__main__':
     myConsole().cmdloop()
