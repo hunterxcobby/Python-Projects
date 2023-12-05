2e38e4da9289713f50cd65b0c4acb75ec1273f3c
hunterxcobby <solomonsefah13@gmail.com>
2023-12-05 23:41:36 +0000

    implement the console for interactive mode

--- my_projects/airbnb_console_test/console.py
+++ my_projects/airbnb_console_test/console.py
@@ -1,14 +1,28 @@
-#!/usr/bin/env python3
-import cmd 
+#!/usr/bin/python3
 
-class myConsole(cmd.Cmd):
-    prompt = "cmd$ "
-    
-    def do_create(self, input):
-        print("I have created",input)
+"""The cmd Module.
+for building line-oriented command interpreters
+"""
+import cmd
 
-    def do_exit(self, ):
-        exit()
+class MyConsole(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
+
+    def do_EOF(self, line):
+        """Exit the console on EOF (Ctrl+D) command"""
+        print("Exiting the console...")
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the console"""
+        return True
+
+    def help_quit(self):
+        print("Command to quit the program.")
 
 if __name__ == '__main__':
-    myConsole().cmdloop()
+    MyConsole().cmdloop()
