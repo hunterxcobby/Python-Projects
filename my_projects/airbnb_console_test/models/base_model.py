5dbdae50491bdb0cb880b1bbd5783a1ebbcf135f
hunterxcobby <solomonsefah13@gmail.com>
2023-12-06 14:00:32 +0000

    link your BaseModel to FileStorage by using the variable storage

--- my_projects/airbnb_console_test/models/base_model.py
+++ my_projects/airbnb_console_test/models/base_model.py
@@ -2,6 +2,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -25,6 +26,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+
 
     def __str__(self) -> str:
         """Return a string representation of the instance."""
@@ -34,6 +37,7 @@ class BaseModel:
     def save(self) -> None:
         """Update the updated_at attribute and save the instance."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self) -> dict:
         """Return a dictionary of instance attributes."""
