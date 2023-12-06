44443bbffe31b6789a7ec84983064274c59f4cfc
hunterxcobby <solomonsefah13@gmail.com>
2023-12-06 07:43:52 +0000

    a class BaseModel that defines all common attributes/methods for other classes::

---
+++ my_projects/airbnb_console_test/models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel class for creating and managing instances.
+    """
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance of BaseModel."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Update the updated_at attribute and save the instance."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
