0a2dd997a2da72f5168a7e78c929c30305b6f813
hunterxcobby <solomonsefah13@gmail.com>
2023-12-06 00:10:19 +0000

    fixed program not running at all

--- my_projects/airbnb_console_test/console.py
+++ my_projects/airbnb_console_test/console.py
@@ -4,6 +4,7 @@
 for building line-oriented command interpreters
 """
 import cmd
+import sys
 
 class MyConsole(cmd.Cmd):
     prompt = "(hbnb) "
@@ -14,7 +15,6 @@ class MyConsole(cmd.Cmd):
 
     def do_EOF(self, line):
         """Exit the console on EOF (Ctrl+D) command"""
-        print("Exiting the console...")
         return True
 
     def do_quit(self, line):
@@ -23,6 +23,28 @@ class MyConsole(cmd.Cmd):
 
     def help_quit(self):
         print("Command to quit the program.")
+    
+    def default(self, line):
+        """
+        Handle unrecognized commands.
+        """
+        print(f"Unrecognized command: {line}. Type 'help' for assistance.")
+
+"""Non-interactive mode.
+To run our console in non-interactive mode
+"""
+
+def run_console():
+    console = MyConsole()
+
+    if not sys.stdin.isatty():
+        # Read commands from standard input in non-interactive mode
+        for line in sys.stdin:
+            console.onecmd(line.strip())
+
+    else:
+        # Run the console interactively
+        console.cmdloop()
 
 if __name__ == '__main__':
-    MyConsole().cmdloop()
+    run_console()
