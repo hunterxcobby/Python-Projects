4d6b3c2329c9ff35df045eacd501a0d365e50775
hunterxcobby <solomonsefah13@gmail.com>
2023-12-06 14:49:56 +0000

    remove calling of method newself and bring it under only when a new instance is created

--- my_projects/airbnb_console_test/models/base_model.py
+++ my_projects/airbnb_console_test/models/base_model.py
@@ -26,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
             
 
 
@@ -37,7 +38,6 @@ class BaseModel:
     def save(self) -> None:
         """Update the updated_at attribute and save the instance."""
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
 
