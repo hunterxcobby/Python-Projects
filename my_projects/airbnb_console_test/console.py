8317f4fc482c1f984a0a36368873e3ba91c0b033
hunterxcobby <solomonsefah13@gmail.com>
2023-12-07 12:49:59 +0000

    a program called console.py that contains the entry point of the command interpreter:

--- my_projects/airbnb_console_test/console.py
+++ my_projects/airbnb_console_test/console.py
@@ -7,23 +7,24 @@ import cmd
 import sys
 
 
-class MyConsole(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def emptyline(self):
         """Do nothing on an empty line"""
         pass
 
-    def do_EOF(self, line):
-        """Exit the console on EOF (Ctrl+D) command"""
-        return True
-
     def do_quit(self, line):
         """Quit command to exit the console"""
         return True
 
+
+    def do_EOF(self, line):
+        """Exit the console on EOF (Ctrl+D) command"""
+        return True
+
     def help_quit(self):
-        print("Command to quit the program.")
+        print("Quit command to exit the program\n")
 
     def default(self, line):
         """
@@ -31,24 +32,5 @@ class MyConsole(cmd.Cmd):
         """
         print(f"Unrecognized command: {line}. Type 'help' for assistance.")
 
-
-"""Non-interactive mode.
-To run our console in non-interactive mode
-"""
-
-
-def run_console():
-    console = MyConsole()
-
-    if not sys.stdin.isatty():
-        # Read commands from standard input in non-interactive mode
-        for line in sys.stdin:
-            console.onecmd(line.strip())
-
-    else:
-        # Run the console interactively
-        console.cmdloop()
-
-
 if __name__ == '__main__':
-    run_console()
+    HBNBCommand().cmdloop()
