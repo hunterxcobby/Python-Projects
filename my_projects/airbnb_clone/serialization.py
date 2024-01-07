544278036d96eec8e65287114f6825b0c39b096c
hunterxcobby <solomonsefah13@gmail.com>
2024-01-07 15:00:28 +0000

    add comment to explain serialziation

--- my_projects/airbnb_clone/serialization.py
+++ my_projects/airbnb_clone/serialization.py
@@ -15,4 +15,12 @@ data = {
 # serialize (convert to json formatted string)
 json_string = json.dumps(data)
 
-print(json_string)
+# save it to a file
+with open("data.json","w") as json_file:
+    json_file.write(json_string)
+
+
+"""
+ It's commonly used for configuration files, data exchange between a server and a web application, and more.
+ In the context of file storage, you can use JSON to serialize (convert to a string) and deserialize (convert back to a Python object) data."""
+
